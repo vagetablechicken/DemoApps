d0f0465e2eb24221c3402086169c91916b481b61
Huang Wei <huangwei@apache.org>
2021-06-17 16:32:37 +0800

    standalone table cycles

--- predict-remaining-useful-life-nb/start_demo.sh
+++ predict-remaining-useful-life-nb/start_demo.sh
@@ -4,3 +4,4 @@
 
 docker run -e GRANT_SUDO=yes --user root  -v `pwd`:/home/jovyan/work/rul -h=`hostname` --network=host -t 4pdosc/fedb_notebook:0.4.0 /bin/bash -c "cd /home/jovyan/work/rul && ls -la  && whoami && pip install -r demo/requirements.txt && jupyter notebook --allow-root"
 
+# TODO(hw): add developer's guide. Normal user just use the image, no mount
